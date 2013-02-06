606ea5d7ef6df867a888a9fe965608c9a67cb9ea
Vasek Purchart <me@vasekpurchart.cz>
2013-02-06 16:05:55 +0100

    transferToTechnician

--- src/main/java/cz/cvut/jboss/storagecycle/Api/Remote/StorageCycleRemoteFacade.java
+++ src/main/java/cz/cvut/jboss/storagecycle/Api/Remote/StorageCycleRemoteFacade.java
@@ -1,6 +1,9 @@
 package cz.cvut.jboss.storagecycle.Api.Remote;
 
+import cz.cvut.jboss.storagecycle.Api.Local.StockNotAvailableException;
 import cz.cvut.jboss.storagecycle.Api.Local.StorageCycleLocalFacade;
+import cz.cvut.jboss.storagecycle.Person.PersonRepository;
+import cz.cvut.jboss.storagecycle.Person.Technician;
 import cz.cvut.jboss.storagecycle.Product.ProductRepository;
 import cz.cvut.jboss.storagecycle.Product.ProductType;
 import javax.ejb.Stateless;
@@ -16,11 +19,21 @@ public class StorageCycleRemoteFacade {
 	@Inject
 	private StorageCycleLocalFacade local;
 
+	@Inject
 	private ProductRepository productRepository;
+
+	@Inject
+	private PersonRepository personRepository;
 
 	public void importToWarehouse(String barcode, int count) {
 		ProductType productType = productRepository.findProductTypeByBarcode(barcode);
 		local.importToWarehouse(productType, count);
+	}
+
+	public void transferToTechnician(String barcode, int count, long technicianId) throws StockNotAvailableException {
+		ProductType productType = productRepository.findProductTypeByBarcode(barcode);
+		Technician technician = personRepository.findTechnicianById(technicianId);
+		local.transferToTechnician(productType, count, technician);
 	}
 
 }
---
+++ src/main/java/cz/cvut/jboss/storagecycle/Person/PersonRepository.java
@@ -0,0 +1,19 @@
+package cz.cvut.jboss.storagecycle.Person;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author vasek
+ */
+public class PersonRepository {
+
+	@Inject
+	private EntityManager em;
+
+	public Technician findTechnicianById(long id) {
+		return em.find(Technician.class, id);
+	}
+
+}
