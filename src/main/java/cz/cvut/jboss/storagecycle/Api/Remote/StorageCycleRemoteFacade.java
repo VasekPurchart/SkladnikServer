04e104899ff18f1fc7b5163a18910f63b6d5e274
Vasek Purchart <me@vasekpurchart.cz>
2013-02-06 15:58:35 +0100

    importToWarehouse

--- src/main/java/cz/cvut/jboss/storagecycle/Api/Remote/StorageCycleRemoteFacade.java
+++ src/main/java/cz/cvut/jboss/storagecycle/Api/Remote/StorageCycleRemoteFacade.java
@@ -1,6 +1,8 @@
 package cz.cvut.jboss.storagecycle.Api.Remote;
 
 import cz.cvut.jboss.storagecycle.Api.Local.StorageCycleLocalFacade;
+import cz.cvut.jboss.storagecycle.Product.ProductRepository;
+import cz.cvut.jboss.storagecycle.Product.ProductType;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -13,5 +15,12 @@ public class StorageCycleRemoteFacade {
 
 	@Inject
 	private StorageCycleLocalFacade local;
+
+	private ProductRepository productRepository;
+
+	public void importToWarehouse(String barcode, int count) {
+		ProductType productType = productRepository.findProductTypeByBarcode(barcode);
+		local.importToWarehouse(productType, count);
+	}
 
 }
